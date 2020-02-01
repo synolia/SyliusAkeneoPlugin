abc7a400c42d4f5e91a2cf1a912456dbcc124cf8
Olivier ALLAIN <olivier.allain@synolia.com>
2020-02-01 18:27:32 +0100

    init akeneo sdk

---
+++ tests/PHPUnit/Api/GetCategoriesTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Synolia\SyliusAkeneoPlugin\PHPUnit\Api;
+
+use Akeneo\Pim\ApiClient\Api\CategoryApi;
+use Akeneo\Pim\ApiClient\Pagination\ResourceCursor;
+use Akeneo\Pim\ApiClient\Pagination\ResourceCursorInterface;
+use donatj\MockWebServer\RequestInfo;
+use donatj\MockWebServer\Response;
+use donatj\MockWebServer\ResponseStack;
+use PHPUnit\Framework\Assert;
+
+class GetCategoriesTest extends ApiTestCase
+{
+    public function testGetCategories(): void
+    {
+        $this->server->setResponseOfPath(
+            '/' . sprintf(CategoryApi::CATEGORIES_URI),
+            new ResponseStack(
+                new Response($this->getCategories(), [], 200)
+            )
+        );
+
+        $api = $this->createClient()->getCategoryApi();
+
+        /** @var ResourceCursorInterface $categories */
+        $categories = $api->all();
+
+        /** @var RequestInfo $lastRequest */
+        $lastRequest = $this->server->getLastRequest();
+        Assert::assertInstanceOf(RequestInfo::class, $lastRequest);
+        Assert::assertSame($lastRequest->jsonSerialize()[RequestInfo::JSON_KEY_METHOD], 'GET');
+        Assert::assertInstanceOf(ResourceCursor::class, $categories);
+        Assert::assertSame(\json_decode($this->getCategories(), true)['_embedded']['items'][0], $categories->current());
+    }
+
+    private function getCategories(): string
+    {
+        return $this->getFileContent('categories.json');
+    }
+}
