5aac6a92697b5d8e851d3ac6e6e3b57d759cf813
Nicolas MELONI <nicolas.meloni@synolia.com>
2020-02-12 16:17:33 +0100

    save token

--- src/Controller/ApiConfigurationController.php
+++ src/Controller/ApiConfigurationController.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Synolia\SyliusAkeneoPlugin\Controller;
 
+use Akeneo\Pim\ApiClient\AkeneoPimClientBuilder;
+use Akeneo\Pim\ApiClient\AkeneoPimClientInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Synolia\SyliusAkeneoPlugin\Entity\ApiConfiguration;
 use Synolia\SyliusAkeneoPlugin\Form\Type\ApiConfigurationType;
 
@@ -15,7 +18,8 @@ final class ApiConfigurationController extends AbstractController
 {
     public function configurationAction(
         Request $request,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        FlashBagInterface $flashBag
     ): Response {
         /** @var ApiConfiguration $apiConfiguration */
         $apiConfiguration = $entityManager->getRepository(ApiConfiguration::class)->findOneBy([]);
@@ -28,14 +32,59 @@ final class ApiConfigurationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var \Symfony\Component\Form\SubmitButton $testCredentialsButton */
+            $testCredentialsButton = $form->get('testCredentials');
+            if ($testCredentialsButton->isClicked()) {
+                try {
+                    $this->testCredentials($apiConfiguration);
+                } catch (\Throwable $throwable) {
+                    $flashBag->add('error', $throwable->getMessage());
+                }
+
+                return $this->render('@SynoliaSyliusAkeneoPlugin/Admin/AkeneoConnector/api_configuration.html.twig', [
+                    'form' => $form->createView(),
+                ]);
+            }
+
+            try {
+                $client = $this->connect($apiConfiguration);
+
+                $apiConfiguration->setToken($client->getToken() ?? '');
+                $apiConfiguration->setRefreshToken($client->getRefreshToken() ?? '');
+            } catch (\Throwable $throwable) {
+                $flashBag->add('error', $throwable->getMessage());
+            }
+
             $entityManager->persist($apiConfiguration);
             $entityManager->flush();
 
-            return $this->redirectToRoute('sylius_akeneo_connector_api_configuration');
+            return $this->render('@SynoliaSyliusAkeneoPlugin/Admin/AkeneoConnector/api_configuration.html.twig', [
+                'form' => $form->createView(),
+            ]);
         }
 
         return $this->render('@SynoliaSyliusAkeneoPlugin/Admin/AkeneoConnector/api_configuration.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    private function connect(ApiConfiguration $apiConfiguration): AkeneoPimClientInterface
+    {
+        $clientBuilder = new AkeneoPimClientBuilder($apiConfiguration->getBaseUrl() ?? '');
+
+        return $clientBuilder->buildAuthenticatedByPassword(
+            $apiConfiguration->getApiClientId() ?? '',
+            $apiConfiguration->getApiClientSecret() ?? '',
+            $apiConfiguration->getUsername() ?? '',
+            $apiConfiguration->getPassword() ?? '',
+        );
+    }
+
+    private function testCredentials(ApiConfiguration $apiConfiguration): void
+    {
+        $client = $this->connect($apiConfiguration);
+
+        $apiConfiguration->setToken($client->getToken() ?? '');
+        $apiConfiguration->setRefreshToken($client->getRefreshToken() ?? '');
     }
 }
--- src/Form/Type/ApiConfigurationType.php
+++ src/Form/Type/ApiConfigurationType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -22,7 +23,7 @@ final class ApiConfigurationType extends AbstractType
         $builder
             ->add('baseUrl', TextType::class)
             ->add('username', TextType::class)
-            ->add('password', TextType::class)
+            ->add('password', PasswordType::class)
             ->add('apiClientId', TextType::class)
             ->add('apiClientSecret', TextType::class)
             ->add('paginationSize', IntegerType::class)
@@ -37,6 +38,7 @@ final class ApiConfigurationType extends AbstractType
                 'by_reference' => false,
             ])
 
+            ->add('testCredentials', SubmitType::class)
             ->add('submit', SubmitType::class)
         ;
     }
