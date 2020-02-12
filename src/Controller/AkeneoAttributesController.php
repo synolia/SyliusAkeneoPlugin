c477a247f653eec806b94135a0b69aa3f53e211a
Luca Gallois <luca.gallois@synolia.com>
2020-02-12 09:12:04 +0100

    fix grumphp

--- src/Controller/AkeneoAttributesController.php
+++ src/Controller/AkeneoAttributesController.php
@@ -1,8 +1,8 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Synolia\SyliusAkeneoPlugin\Controller;
-
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
--- src/Menu/AdminMenuListener.php
+++ src/Menu/AdminMenuListener.php
@@ -17,48 +17,48 @@ final class AdminMenuListener
         $newSubmenu = $menu->addChild('akeneo');
 
         $newSubmenu->addChild('sylius_admin_akeneo_api_configuration', [
-            'route' => 'sylius_akeneo_connector_api_configuration',
-        ])
+                'route' => 'sylius_akeneo_connector_api_configuration',
+            ])
             ->setAttribute('type', 'link')
             ->setLabel('sylius.ui.admin.akeneo.api_configuration.api_configuration')
             ->setLabelAttribute('icon', 'cogs')
         ;
 
         $newSubmenu->addChild('sylius_admin_akeneo_product_filter_rules', [
-            'route' => 'sylius_akeneo_connector_product_filter_rules',
-        ])
+                'route' => 'sylius_akeneo_connector_product_filter_rules',
+            ])
             ->setAttribute('type', 'link')
             ->setLabel('sylius.ui.admin.akeneo.product_filter_rules.product_filter_rules')
             ->setLabelAttribute('icon', 'sync alternate')
         ;
 
         $newSubmenu->addChild('sylius_admin_akeneo_categories', [
-            'route' => 'sylius_akeneo_connector_categories',
-        ])
+                'route' => 'sylius_akeneo_connector_categories',
+            ])
             ->setAttribute('type', 'link')
             ->setLabel('sylius.ui.admin.akeneo.categories.categories')
             ->setLabelAttribute('icon', 'configure')
         ;
 
         $newSubmenu->addChild('sylius_admin_akeneo_products', [
-            'route' => 'sylius_akeneo_connector_products',
-        ])
+                'route' => 'sylius_akeneo_connector_products',
+            ])
             ->setAttribute('type', 'link')
             ->setLabel('sylius.ui.admin.akeneo.products.products')
             ->setLabelAttribute('icon', 'configure')
         ;
 
         $newSubmenu->addChild('sylius_admin_akeneo_attributes', [
-            'route' => 'sylius_akeneo_connector_attributes',
-        ])
+                'route' => 'sylius_akeneo_connector_attributes',
+            ])
             ->setAttribute('type', 'link')
             ->setLabel('sylius.ui.admin.akeneo.attributes.attributes')
             ->setLabelAttribute('icon', 'configure')
         ;
 
         $newSubmenu->addChild('sylius_admin_akeneo_medias', [
-            'route' => 'sylius_akeneo_connector_medias',
-        ])
+                'route' => 'sylius_akeneo_connector_medias',
+            ])
             ->setAttribute('type', 'link')
             ->setLabel('sylius.ui.admin.akeneo.medias.medias')
             ->setLabelAttribute('icon', 'image')
