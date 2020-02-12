0f769509c4829bb684d04469d0c534e0a6321e0a
Nicolas MELONI <nicolas.meloni@synolia.com>
2020-02-12 14:37:18 +0100

    add configuration entity

--- src/Controller/AttributesController.php
+++ src/Controller/AttributesController.php
@@ -4,13 +4,38 @@ declare(strict_types=1);
 
 namespace Synolia\SyliusAkeneoPlugin\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Synolia\SyliusAkeneoPlugin\Entity\ApiConfiguration;
+use Synolia\SyliusAkeneoPlugin\Form\Type\ApiConfigurationType;
 
 final class AttributesController extends AbstractController
 {
-    public function __invoke(): Response
-    {
-        throw new \LogicException('Not implemented yet');
+    public function configurationAction(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
+        /** @var ApiConfiguration $apiConfiguration */
+        $apiConfiguration = $entityManager->getRepository(ApiConfiguration::class)->findOneBy([]);
+
+        if (!$apiConfiguration instanceof ApiConfiguration) {
+            $apiConfiguration = new ApiConfiguration();
+        }
+
+        $form = $this->createForm(ApiConfigurationType::class, $apiConfiguration);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($apiConfiguration);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('sylius_akeneo_connector_api_configuration');
+        }
+
+        return $this->render('@SynoliaSyliusAkeneoPlugin/Admin/AkeneoConnector/api_configuration.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
