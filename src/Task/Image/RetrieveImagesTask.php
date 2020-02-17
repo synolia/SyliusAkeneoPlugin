0a3f917edd5d5b4bb5e05d1ce3e576a3c3e44304
Nicolas MELONI <nicolas.meloni@synolia.com>
2020-02-17 16:17:13 +0100

    added image tasks

--- src/Task/Image/RetrieveImagesTask.php
+++ src/Task/Image/RetrieveImagesTask.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Synolia\SyliusAkeneoPlugin\Task\Product;
+namespace Synolia\SyliusAkeneoPlugin\Task\Image;
 
 use Synolia\SyliusAkeneoPlugin\Model\PipelinePayloadInterface;
 use Synolia\SyliusAkeneoPlugin\Task\AkeneoTaskInterface;
 
-final class CreateProductEntityTask implements AkeneoTaskInterface
+final class RetrieveImagesTask implements AkeneoTaskInterface
 {
     public function __invoke(PipelinePayloadInterface $payload): PipelinePayloadInterface
     {
