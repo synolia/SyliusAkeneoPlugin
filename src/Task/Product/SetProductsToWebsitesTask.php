e9ba58051692f1f4e808d500c88467c3411951b4
Nicolas MELONI <nicolas.meloni@synolia.com>
2020-02-17 16:16:56 +0100

    added product tasks

---
+++ src/Task/Product/SetProductsToWebsitesTask.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Synolia\SyliusAkeneoPlugin\Task\Product;
+
+use Synolia\SyliusAkeneoPlugin\Model\PipelinePayloadInterface;
+use Synolia\SyliusAkeneoPlugin\Task\AkeneoTaskInterface;
+
+final class SetProductsToWebsitesTask implements AkeneoTaskInterface
+{
+    public function __invoke(PipelinePayloadInterface $payload): PipelinePayloadInterface
+    {
+        return $payload;
+    }
+}
