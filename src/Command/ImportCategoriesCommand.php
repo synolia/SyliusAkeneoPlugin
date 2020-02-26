22b0a4cc44a9a6955343fc5f31fb33eab71a8b34
Nicolas MELONI <nicolas.meloni@synolia.com>
2020-02-26 09:47:26 +0100

    added symfony/lock

--- src/Command/ImportCategoriesCommand.php
+++ src/Command/ImportCategoriesCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Synolia\SyliusAkeneoPlugin\Command;
 
 use Allocine\Twigcs\Console\ContainerAwareCommand;
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Synolia\SyliusAkeneoPlugin\Client\ClientFactory;
@@ -13,6 +14,8 @@ use Synolia\SyliusAkeneoPlugin\Payload\Category\CategoryPayload;
 
 final class ImportCategoriesCommand extends ContainerAwareCommand
 {
+    use LockableTrait;
+
     protected static $defaultName = 'akeneo:import:categories';
 
     /** @var \Synolia\SyliusAkeneoPlugin\Factory\CategoryPipelineFactory */
@@ -38,12 +41,20 @@ final class ImportCategoriesCommand extends ContainerAwareCommand
         InputInterface $input,
         OutputInterface $output
     ) {
+        if (!$this->lock()) {
+            $output->writeln('The command is already running in another process.');
+
+            return 0;
+        }
+
         /** @var \League\Pipeline\Pipeline $categoryPipeline */
         $categoryPipeline = $this->categoryPipelineFactory->create();
 
         /** @var \Synolia\SyliusAkeneoPlugin\Payload\Category\CategoryPayload $categoryPayload */
         $categoryPayload = new CategoryPayload($this->clientFactory->createFromApiCredentials());
         $categoryPipeline->process($categoryPayload);
+
+        $this->release();
 
         return 0;
     }
