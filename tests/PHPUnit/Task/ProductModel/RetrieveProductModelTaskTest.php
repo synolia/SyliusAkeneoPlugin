af50e8d1ee974ecc8315903fd335b054d4c6f149
Luca Gallois <luca.gallois@synolia.com>
2020-03-16 10:31:32 +0100

    test import product model

--- tests/PHPUnit/Task/ProductModel/RetrieveProductModelTaskTest.php
+++ tests/PHPUnit/Task/ProductModel/RetrieveProductModelTaskTest.php
@@ -6,13 +6,11 @@ namespace Tests\Synolia\SyliusAkeneoPlugin\PHPUnit\Task\ProductModel;
 
 use donatj\MockWebServer\MockWebServer;
 use PHPUnit\Framework\Assert;
-use Synolia\SyliusAkeneoPlugin\Model\PipelinePayloadInterface;
 use Synolia\SyliusAkeneoPlugin\Payload\ProductModel\ProductModelPayload;
 use Synolia\SyliusAkeneoPlugin\Provider\AkeneoTaskProvider;
-use Synolia\SyliusAkeneoPlugin\Task\ProductModel\AddOrUpdateProductModelsTask;
 use Synolia\SyliusAkeneoPlugin\Task\ProductModel\RetrieveProductModelsTask;
 
-final class AddOrUpdateProductModelTaskTest extends AbstractTaskTest
+final class RetrieveProductModelTaskTest extends AbstractTaskTest
 {
     /** @var AkeneoTaskProvider */
     private $taskProvider;
@@ -34,11 +32,7 @@ final class AddOrUpdateProductModelTaskTest extends AbstractTaskTest
 
         /** @var RetrieveProductModelsTask $retrieveProductModelsTask */
         $retrieveProductModelsTask = $this->taskProvider->get(RetrieveProductModelsTask::class);
-        $optionsPayload = $retrieveProductModelsTask->__invoke($productModelPayload);
-
-        /** @var AddOrUpdateProductModelsTask $addOrUpdateProductModelsTask */
-        $addOrUpdateProductModelsTask = $this->taskProvider->get(AddOrUpdateProductModelsTask::class);
-        $result = $addOrUpdateProductModelsTask->__invoke($optionsPayload);
-        Assert::assertInstanceOf(PipelinePayloadInterface::class, $result);
+        $productModelPayload = $retrieveProductModelsTask->__invoke($productModelPayload);
+        Assert::assertNotEmpty($productModelPayload->getResources());
     }
 }
