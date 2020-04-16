381fd6c13c39d8b3d70d6f026bdf7ce0361ce71a
maxperei <maxime.pereira@synolia.com>
2020-04-16 12:25:39 +0200

    SYLIUS-19 [front] BO product - afficher/masquer les champs en fonction d'un choix prédéfini

--- assets/src/js/admin.js
+++ assets/src/js/admin.js
@@ -8,24 +8,79 @@ const Admin = {
    * {jQuery}
    */
   trigger: $("#switchForm, #switchForm + label"),
+  /**
+   * {Object}
+   */
+  options: {
+    locale: {
+      trigger: "#product_filter_rule_simple_completeness_type",
+      selector: "#product_filter_rule_simple_locales",
+      values: ["6", "7", "8", "9"],
+    },
+    before: {
+      trigger: "#product_filter_rule_simple_updated_mode",
+      selector: "#product_filter_rule_simple_updated_before",
+      values: ["BETWEEN", "<"],
+    },
+    after: {
+      trigger: "#product_filter_rule_simple_updated_mode",
+      selector: "#product_filter_rule_simple_updated_after",
+      values: ["BETWEEN", ">"],
+    },
+    since: {
+      trigger: "#product_filter_rule_simple_updated_mode",
+      selector: "#product_filter_rule_simple_updated",
+      values: ["SINCE LAST N DAYS"],
+    },
+  },
+  /**
+   * @return {void}
+   */
   initialize() {
     $(document).ready(() => {
-      this.credits();
+      this.copyrights();
       if (window.location.href.includes("/product_filter/rules")) {
         this.toggleForms();
+        this.toggleFields();
       }
     });
   },
-  credits() {
+  /**
+   * @return {void}
+   */
+  copyrights() {
     for (let i = 0; i < this.organizations.length; i++) {
       $(`a[href*=${this.organizations[i]}]`).addClass(this.organizations[i]);
     }
   },
+  /**
+   * @return {void}
+   */
   toggleForms() {
     this.trigger.removeClass("hidden");
     this.trigger.on("click", function () {
       $(".togglable").toggle();
     });
+  },
+  /**
+   * @return {void}
+   */
+  toggleFields() {
+    const self = this;
+    $(`
+      ${this.options.locale.selector}, 
+      ${this.options.after.selector},
+      ${this.options.since.selector}
+    `)
+      .parent(".field")
+      .addClass("hidden");
+    for (let k in this.options) {
+      $(this.options[k].trigger).on("change", function () {
+        self.options[k].values.includes($(this).val())
+          ? $(self.options[k].selector).parent(".field").removeClass("hidden")
+          : $(self.options[k].selector).parent(".field").addClass("hidden");
+      });
+    }
   },
 };
 
