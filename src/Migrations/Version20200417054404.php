be182107c11c25c183e3caf2f88bee39b2c8d985
Nicolas MELONI <nicolas.meloni@synolia.com>
2020-04-21 09:08:49 +0200

    merged migrations

--- src/Migrations/Version20200417054404.php
+++ src/Migrations/Version20200417054404.php
@@ -14,7 +14,8 @@ final class Version20200417054404 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Renamed akeneo attribute to akeneo attribute type.';
+        return 'Renamed akeneo attribute to akeneo attribute type.
+        Makes akeneo attribute type unique on attribute type mapping table.';
     }
 
     public function up(Schema $schema): void
@@ -24,6 +25,7 @@ final class Version20200417054404 extends AbstractMigration
 
         $this->addSql('ALTER TABLE 
           akeneo_attribute_type_mapping CHANGE akeneoattribute akeneoAttributeType VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_FF5E270FA2851109 ON akeneo_attribute_type_mapping (akeneoAttributeType)');
     }
 
     public function down(Schema $schema): void
@@ -31,6 +33,7 @@ final class Version20200417054404 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('DROP INDEX UNIQ_FF5E270FA2851109 ON akeneo_attribute_type_mapping');
         $this->addSql('ALTER TABLE 
           akeneo_attribute_type_mapping CHANGE akeneoattributetype akeneoAttribute VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`');
     }
