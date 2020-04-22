b81e3212aff64d7e48d04b7cd59e3bf419259c43
Nicolas MELONI <nicolas.meloni@synolia.com>
2020-04-22 09:56:49 +0200

    removed unused namespace after rebase

--- src/Form/Type/ProductConfigurationType.php
+++ src/Form/Type/ProductConfigurationType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 final class ProductConfigurationType extends AbstractType
