31bfdebe544486f094cf02ec744b21e2b166d73c
Luca Gallois <luca.gallois@synolia.com>
2020-05-26 17:32:32 +0200

    Add attribute type form with multiselect

---
+++ src/Form/Type/AttributeTypeChoiceType.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Synolia\SyliusAkeneoPlugin\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class AttributeTypeChoiceType extends AbstractType
+{
+    /** @var array */
+    private $attributeTypes;
+
+    public function __construct(array $attributeTypes)
+    {
+        $this->attributeTypes = $attributeTypes;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefault('choices', array_merge($this->attributeTypes, ['multiselect' => 'multiselect']));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent(): string
+    {
+        return ChoiceType::class;
+    }
+}
