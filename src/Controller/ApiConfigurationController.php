5a98c605628c1e79c33a9fb4bff19f9c76bb29c0
Nicolas MELONI <nicolas.meloni@synolia.com>
2022-07-29 10:58:19 +0200

    add api configuration from env

--- src/Controller/ApiConfigurationController.php
+++ src/Controller/ApiConfigurationController.php
@@ -15,6 +15,7 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 use Synolia\SyliusAkeneoPlugin\Client\ClientFactoryInterface;
 use Synolia\SyliusAkeneoPlugin\Entity\ApiConfiguration;
 use Synolia\SyliusAkeneoPlugin\Form\Type\ApiConfigurationType;
+use Webmozart\Assert\Assert;
 
 /**
  * @deprecated To be removed in 4.0.
@@ -69,6 +70,7 @@ final class ApiConfigurationController extends AbstractController
             $testCredentialsButton = $form->get('testCredentials');
 
             try {
+                Assert::isInstanceOf($apiConfiguration, ApiConfiguration::class);
                 $client = $this->clientFactory->authenticateByPassword($apiConfiguration);
                 $client->getCategoryApi()->all(self::PAGING_SIZE);
 
