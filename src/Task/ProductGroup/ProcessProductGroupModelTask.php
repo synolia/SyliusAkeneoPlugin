1b536729ea88c60e0efc3a96417589f1e47cd114
Nicolas MELONI <nicolas.meloni@synolia.com>
2022-10-03 17:50:52 +0200

    sync product group associations

---
+++ src/Task/ProductGroup/ProcessProductGroupModelTask.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Synolia\SyliusAkeneoPlugin\Task\ProductGroup;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Synolia\SyliusAkeneoPlugin\Entity\ProductGroupInterface;
+use Synolia\SyliusAkeneoPlugin\Payload\PipelinePayloadInterface;
+use Synolia\SyliusAkeneoPlugin\Provider\Configuration\Api\ApiConnectionProviderInterface;
+use Synolia\SyliusAkeneoPlugin\Repository\ProductGroupRepository;
+use Synolia\SyliusAkeneoPlugin\Task\AkeneoTaskInterface;
+
+class ProcessProductGroupModelTask implements AkeneoTaskInterface
+{
+    private ApiConnectionProviderInterface $apiConnectionProvider;
+
+    private ProductGroupRepository $productGroupRepository;
+
+    private ProductRepositoryInterface $productRepository;
+
+    private LoggerInterface $logger;
+
+    private EntityManagerInterface $entityManager;
+
+    private array $productGroups;
+
+    public function __construct(
+        ApiConnectionProviderInterface $apiConnectionProvider,
+        ProductGroupRepository $productGroupRepository,
+        ProductRepositoryInterface $productRepository,
+        LoggerInterface $logger,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->apiConnectionProvider = $apiConnectionProvider;
+        $this->productGroupRepository = $productGroupRepository;
+        $this->productRepository = $productRepository;
+        $this->logger = $logger;
+        $this->entityManager = $entityManager;
+        $this->productGroups = [];
+    }
+
+    public function __invoke(PipelinePayloadInterface $payload): PipelinePayloadInterface
+    {
+        $resourceCursor = $payload->getAkeneoPimClient()->getProductModelApi()->all(
+            $this->apiConnectionProvider->get()->getPaginationSize(),
+        );
+
+        /**
+         * @var array{
+         *     parent: string|null,
+         *     code: string,
+         * } $resource
+         */
+        foreach ($resourceCursor as $resource) {
+            if (null === $resource['parent']) {
+                continue;
+            }
+
+            /** @var ProductGroupInterface|null $productGroup */
+            $productGroup = $this->productGroupRepository->findOneBy(['productParent' => $resource['parent']]);
+
+            if (!$productGroup instanceof ProductGroupInterface) {
+                continue;
+            }
+
+            /** @var ProductInterface|null $product */
+            $product = $this->productRepository->findOneByCode($resource['code']);
+
+            if (!$product instanceof ProductInterface) {
+                continue;
+            }
+
+            if (!\array_key_exists($productGroup->getProductParent(), $this->productGroups)) {
+                $this->productGroups[$productGroup->getProductParent()] = true;
+                $productGroup->getProducts()->clear();
+
+                $this->logger->info('Cleaned ProductGroup associations', [
+                    'product_code' => $product->getCode(),
+                    'product_group_id' => $productGroup->getId(),
+                    'family' => $productGroup->getFamily(),
+                ]);
+            }
+
+            $productGroup->addProduct($product);
+            $this->entityManager->flush();
+
+            $this->logger->info('Added product to ProductGroup association', [
+                'product_code' => $product->getCode(),
+                'product_group_id' => $productGroup->getId(),
+                'family' => $productGroup->getFamily(),
+            ]);
+        }
+
+        return $payload;
+    }
+}
---
+++ tests/PHPUnit/Task/ProductGroup/ProcessProductGroupModelTaskTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Synolia\SyliusAkeneoPlugin\PHPUnit\Task\ProductGroup;
+
+use PHPUnit\Framework\Assert;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Sylius\Component\Core\Model\Product;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Synolia\SyliusAkeneoPlugin\Entity\ProductConfiguration;
+use Synolia\SyliusAkeneoPlugin\Entity\ProductGroup;
+use Synolia\SyliusAkeneoPlugin\Payload\ProductModel\ProductModelPayload;
+use Synolia\SyliusAkeneoPlugin\Provider\AkeneoAttributePropertiesProvider;
+use Synolia\SyliusAkeneoPlugin\Provider\TaskProvider;
+use Synolia\SyliusAkeneoPlugin\Task\ProductGroup\ProcessProductGroupModelTask;
+use Synolia\SyliusAkeneoPlugin\Task\ProductModel\ProcessProductModelsTask;
+use Synolia\SyliusAkeneoPlugin\Task\SetupTask;
+use Synolia\SyliusAkeneoPlugin\Task\TearDownTask;
+use Tests\Synolia\SyliusAkeneoPlugin\PHPUnit\Task\ProductModel\AbstractTaskTest;
+
+class ProcessProductGroupModelTaskTest extends AbstractTaskTest
+{
+    private TaskProvider $taskProvider;
+
+    private ProductRepositoryInterface $productRepository;
+
+    private EntityRepository $productGroupRepository;
+
+    private ProcessProductGroupModelTask $processProductGroupModelTask;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        /** @var AkeneoAttributePropertiesProvider $akeneoPropertiesProvider */
+        $akeneoPropertiesProvider = $this->getContainer()->get(AkeneoAttributePropertiesProvider::class);
+        $akeneoPropertiesProvider->setLoadsAllAttributesAtOnce(true);
+        $this->taskProvider = $this->getContainer()->get(TaskProvider::class);
+        $this->productRepository = $this->getContainer()->get('sylius.repository.product');
+        $this->productGroupRepository = $this->getContainer()->get('akeneo.repository.product_group');
+        $this->processProductGroupModelTask = $this->getContainer()->get(ProcessProductGroupModelTask::class);
+        self::assertInstanceOf(TaskProvider::class, $this->taskProvider);
+    }
+
+    public function testCreateProductGroupAssociations(): void
+    {
+        $this->prepareConfiguration();
+
+        $productModelPayload = new ProductModelPayload($this->createClient());
+        $productModelPayload->setProcessAsSoonAsPossible(false);
+
+        $setupProductModelsTask = $this->taskProvider->get(SetupTask::class);
+        $productModelPayload = $setupProductModelsTask->__invoke($productModelPayload);
+
+        /** @var ProcessProductModelsTask $processProductModelsTask */
+        $processProductModelsTask = $this->taskProvider->get(ProcessProductModelsTask::class);
+        $productModelPayload = $processProductModelsTask->__invoke($productModelPayload);
+
+        $tearDownProductModelTask = $this->taskProvider->get(TearDownTask::class);
+        $tearDownProductModelTask->__invoke($productModelPayload);
+
+        /** @var Product $productFinal */
+        $productFinal = $this->productRepository->findOneBy(['code' => 'apollon_yellow']);
+        Assert::assertInstanceOf(Product::class, $productFinal);
+
+        $productGroup = $this->productGroupRepository->findOneBy(['productParent' => 'apollon']);
+        Assert::assertInstanceOf(ProductGroup::class, $productGroup);
+
+        Assert::assertGreaterThan(0, $productGroup->getProducts()->count());
+        $productGroup->getProducts()->clear();
+        Assert::assertEquals(0, $productGroup->getProducts()->count());
+
+        $this->processProductGroupModelTask->__invoke($productModelPayload);
+        Assert::assertGreaterThan(0, $productGroup->getProducts()->count());
+        Assert::assertEquals('apollon_yellow', $productGroup->getProducts()->first()->getCode());
+    }
+
+    private function prepareConfiguration(): void
+    {
+        $productConfiguration = new ProductConfiguration();
+        $this->manager->persist($productConfiguration);
+        $this->manager->flush();
+    }
+}
