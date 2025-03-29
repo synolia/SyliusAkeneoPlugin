c59d85aa396c5a08005cfc24d3893f67609116b3
maxperei <maxime.pereira@synolia.com>
2025-03-29 18:07:44 +0100

    refactor(di): transpose `AttributeType` services yaml into attributes

--- src/Component/Attribute/AttributeType/MetricAttributeType.php
+++ src/Component/Attribute/AttributeType/MetricAttributeType.php
@@ -6,8 +6,22 @@ namespace Synolia\SyliusAkeneoPlugin\Component\Attribute\AttributeType;
 
 use Sylius\Component\Attribute\AttributeType\AttributeTypeInterface;
 use Sylius\Component\Attribute\Model\AttributeValueInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsAlias;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Synolia\SyliusAkeneoPlugin\Component\Attribute\AttributeType\Configuration\MetricConfigurationAttributeType as ConfigurationFormType;
+use Synolia\SyliusAkeneoPlugin\Form\Type\AttributeType\MetricAttributeType as FormType;
 
+#[AsAlias(id: 'sylius.attribute_type.metric')]
+#[AutoconfigureTag(
+    name: 'sylius.attribute.type',
+    attributes: [
+        'attribute_type' => self::TYPE,
+        'label' => 'json',
+        'form_type' => FormType::class,
+        'configuration_form_type' => ConfigurationFormType::class,
+    ],
+)]
 final class MetricAttributeType implements AttributeTypeInterface
 {
     public const TYPE = 'metric';
