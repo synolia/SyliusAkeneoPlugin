a3bfb263d3a50814d304e5ae93950dc468ac3599
maxperei <maxime.pereira@synolia.com>
2025-06-10 16:59:06 +0200

    fix(tests): [kernel] rm useless method

--- tests/PHPUnit/AbstractKernelTestCase.php
+++ tests/PHPUnit/AbstractKernelTestCase.php
@@ -5,12 +5,7 @@ declare(strict_types=1);
 namespace Tests\Synolia\SyliusAkeneoPlugin\PHPUnit;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 abstract class AbstractKernelTestCase extends KernelTestCase
 {
-    protected static function getContainer(): ContainerInterface
-    {
-        return self::$kernel->getContainer();
-    }
 }
